9a330b666971e2f8e09c3c0e607a9984eb759838
learning <learning@DIN66009698.corp.capgemini.com>
2018-07-10 10:23:04 +0530

    Version 1.0 Shruti

---
+++ GitExample/src/main/java/com/cg/GitExample/GitExample/App.java
@@ -0,0 +1,13 @@
+package com.cg.GitExample.GitExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Shruti Agarwal !!" );
+    }
+}
